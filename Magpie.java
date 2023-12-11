5e86c4676bf8f3afb1974392ddc6d053b7c51511
Allen U <91634751+AsianBananaMan134@users.noreply.github.com>
2023-12-11 10:39:41 -0800

    Update Magpie.java

--- Magpie.java
+++ Magpie.java
@@ -8,7 +8,7 @@ public class Magpie
 	{
 		return "Hey, what's up?";
 	}
-	
+
 	/**
 	 * Resturns a response to a user statement
 	 * 
@@ -21,7 +21,8 @@ public class Magpie
 		if (findKeyword(statement, "no") >= 0)
 		{
 		    response = "Don't be so negative!";
-		} else if (
+		} 
+		else if (
 		    findKeyword(statement, "mother") >= 0 ||
 		    findKeyword(statement, "brother") >= 0 ||
 		    findKeyword(statement, "sister") >= 0 ||
@@ -29,7 +30,8 @@ public class Magpie
 		)
 	    {
 	        response = "Tell me more about your family!";
-	    } else if (
+	    } 
+	    else if (
 	        findKeyword(statement, "weather") >= 0 ||
 		    findKeyword(statement, "sun") >= 0 ||
 		    findKeyword(statement, "rain") >= 0
@@ -37,33 +39,34 @@ public class Magpie
         {
             response = "The weather here is really nice.";
         } 
-        else if (
-            findKeyword(statement, "dog") >= 0 ||
-		    findKeyword(statement, "cat") >= 0 ||
-		    findKeyword(statement, "pet") >= 0
-            )
+        else if (findKeyword(statement, "I want to", 0) >= 0)
         {
-		    response = "What do your pets eat";
-	    } 
-        else if (
-            findKeyword(statement, "teacher") >= 0 || 
-            findKeyword(statement, "Teacher") >= 0
-            )
-        { 
-	        response = "Teacher is kool";
+            response = transformIWantToStatement(statement);  
         } 
-        else {
-            response = randomResponse();
+        else if (findKeyword(statement, "I want", 0) >= 0)
+        {
+            response = transformWantStatement(statement);
+        } else {
+            if (hasXThenY(statement, "you", "me"))
+            {
+                response = transformYouMeStatement(statement);
+            } 
+            else if (hasXThenY(statement, "I", "you"))
+            {
+                response = transformIYouStatement(statement);
+            } else {
+                response = randomResponse();
+            }
         }
 		return response;
 	}
-	
+
 	public int findKeyword(String statement, String goal, int startPos)
 	{
 	    String phrase = statement.trim().toLowerCase();
 	    goal = goal.toLowerCase();
 	    int position = phrase.indexOf(goal, startPos);
-	    
+
 	    while (position >= 0)
 	    {
 	        String before = " ";
@@ -88,11 +91,69 @@ public class Magpie
 	    }
 	    return -1;
 	}
-	
-	public int findKeyword(String statement, String goal)
+
+
+	private int findKeyword(String statement, String goal)
 	{
 	    return findKeyword(statement, goal, 0);
 	}
+
+	private String transformIWantToStatement(String statement)
+	{
+	    return transformSingle(statement, "I want to ", "I don't want to ", ".");
+	}
+
+	private String transformWantStatement(String statement)
+	{
+	    return transformSingle(statement, "I want ", "Would you really be happy if you had ", "?");
+	}
+	
+
+	private String transformSingle(String statement, String keyword, String before, String after)
+	{
+	    statement = statement.trim();
+	    String lastChar = statement.substring(statement.length() - 1);
+	    if (lastChar.equals("."))
+	    {
+	        statement = statement.substring(0, statement.length() - 1);
+	    }
+	    int position = findKeyword(statement, keyword, 0);
+	    String restOfStatement = statement.substring(position + keyword.length()).trim();
+	    return before + restOfStatement + after;
+	}
+
+
+	private boolean hasXThenY(String statement, String first, String second)
+	{
+	    int position = findKeyword(statement, first, 0);
+	    return position >= 0 && findKeyword(statement, second, position) >= 0;
+ 	}
+
+ 	private String transformIYouStatement(String statement)
+ 	{
+ 	    return transformInner(statement, "I", "you", "Why do you ", " me?");
+ 	}
+
+ 	private String transformYouMeStatement(String statement)
+ 	{
+ 	    return transformInner(statement, "you", "me", "I don't ", " you, where did you hear that?");
+ 	}
+
+ 	private String transformInner(String statement, String starter, String ender, String before, String after)
+ 	{
+ 	    statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement.length() - 1);
+		}
+
+		int psnOfStart = findKeyword (statement, starter, 0);
+		int psnOfEnd = findKeyword (statement, ender, psnOfStart + starter.length());
+
+		String restOfStatement = statement.substring(psnOfStart + starter.length(), psnOfEnd).trim();
+		return before + restOfStatement + after;
+ 	}
 
 	/**
 	 * Pick a default response to use if nothing else fits.
@@ -104,7 +165,7 @@ public class Magpie
 		double responseIndex = Math.random();
 		int whichResponse = (int)(responseIndex * NUMBER_OF_RESPONSES);
 		String response = "";
-		
+
 		if (whichResponse == 0)
 		{
 			response = "Very cool!";
